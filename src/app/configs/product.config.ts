72ff4168d31cef46ffbf2be6ec4bf59cfbe3bf9e
arorayashika9 <arorayashika9@gmail.com>
2018-04-25 15:12:54 +0530

    functionalities added | produt page| homepage|config|service

--- src/app/configs/product.config.ts
+++ src/app/configs/product.config.ts
@@ -1,3 +1,5 @@
 export class Product{
-public static mapping="http://10.151.61.152:8765/api/offer-database-service/vendor/";
+public static mapping="http://10.151.61.121:9002/vendor/";
+    public static getOfferByOfferId="http://10.151.61.121:9002/offers/"
+
 }
