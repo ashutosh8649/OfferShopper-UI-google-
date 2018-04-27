8f8294a88389dd8374b1d7d029af56aedd6acb9e
Anish <anishkap7010@gmail.com>
2018-04-28 03:18:32 +0530

    Removed unused components

--- src/app/components/home-page/products-list/products-list.component.ts
+++ src/app/components/home-page/products-list/products-list.component.ts
@@ -1,28 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SearchService } from '../../../services/search.service';
+import { OffersService } from '../../../services/offers.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css'],
-  providers: [SearchService]
+  providers: [SearchService, OffersService]
 })
 export class ProductsListComponent implements OnInit {
 
-  @Input() results: any;
-  public products:any=[];
+  public offers : any;
 
-
-  constructor(private searchService : SearchService) { }
+  constructor(private searchService : SearchService,
+  private offersService : OffersService) { }
 
   ngOnInit() {
+    this.loadOffers();
   }
 
-  //searching the products in backend
-  searchResults(result) {
-    this.searchService.searchProducts(result)
-    .subscribe(res => {
-      this.products = res;
+  loadOffers(){
+    this.offersService.getOffers("pooja@gmail.com")
+    .subscribe((res) =>{
+      this.offers=res;
+     },(error) =>{
     });
-  }
+}
 }
--- src/app/components/home-page/search-component/search-component.component.ts
+++ src/app/components/home-page/search-component/search-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../../../services/search.service';
 import { Subject } from 'rxjs/Subject';
 
@@ -11,24 +11,13 @@ import { Subject } from 'rxjs/Subject';
 
 export class SearchComponentComponent implements OnInit {
 
-  //event created to pass category and query to parent class
-  @Output() success = new EventEmitter<any>();
-
-  //true if search has been done
-  searchFlag : boolean = false;
-  //true if results were found
-  resultsFlag : boolean = false;
-
   public results:any=[];
-  public products:any=[];
-  name:String;
 
-  val = new Subject<string>();
   flag :boolean;
   searchTerm$ = new Subject<string>();
 
-  category : string;
-  query : string;
+  category : string = "";
+  query : string = "";
 
   constructor(private searchService: SearchService) {
     //searching the keyword in redis database
@@ -44,87 +33,10 @@ export class SearchComponentComponent implements OnInit {
           this.flag=false;
         }
       });
-    }
+  }
 }
 
   ngOnInit() {
     this.category = 'All';
   }
-
-  showValue() : void {
-    //no results shown
-    this.searchFlag = true;
-  if(this.category=="All" && this.query == null) {
-    alert("Please select a category or search for a deal.");
-  }
-
-  // category based search
-  else if(this.category == "All" && this.query != null) {
-    this.searchService.searchProducts(this.query)
-    .subscribe(res => {
-      this.products = res;
-    });
-    this.flag=false;
-     }  
-
-  else if(this.category != "All" && this.query == null) {
-    this.searchService.searchProductsCategoryOnly(this.category)
-    .subscribe(res => {
-      this.products = res;
-    });
-    this.flag=false;
-  }
-  //search by both category and key
-  else {
-    this.searchService.searchProductsCategoryAndKey(this.category, this.query)
-    .subscribe(res => {
-      this.products = res;
-    });
-    this.flag=false;
-  }
-  }
-
-  //searching the products in backend
-  onclick(result) {
-    this.query=result;
-    this.searchService.searchProducts(result)
-    .subscribe(res => {
-      this.products = res;
-    });
-    this.flag=false;
-  }
-
-  //sorting 
-  sorters = {
-    byPrice: function(firstProduct, secondProduct) {
-      return firstProduct.originalPrice - secondProduct.originalPrice;
-    },
-    byDiscount: function(firstProduct, secondProduct) {
-      return firstProduct.discount - secondProduct.discount;
-    }
-  };
-
-  //function for chosing on which basis to sort from
-  sortBy(x) {
-    switch (x) {
-      case "priceLH":
-        this.products.sort(this.sorters.byPrice);
-        break;
-
-      case "priceHL":
-        this.products.sort(this.sorters.byPrice);
-        this.products.reverse();
-        break;
-
-      case "discountLH":
-        this.products.sort(this.sorters.byDiscount);
-        break;
-
-      case "discountHL":
-        this.products.sort(this.sorters.byDiscount);
-        this.products.reverse();
-        break;
-    }
-  }
-
 }
