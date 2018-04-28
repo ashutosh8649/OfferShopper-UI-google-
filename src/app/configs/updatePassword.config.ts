eed75980a7dac21567e926a0f468328dce6558d1
Pooja <poojapathak0001@gmail.com>
2018-04-28 14:24:36 +0530

    updated api in config files

--- src/app/configs/updatePassword.config.ts
+++ src/app/configs/updatePassword.config.ts
@@ -1,4 +1,4 @@
 export class updatePassword{
-	public static updatePassEmailId="http://10.151.61.152:8765/uaa-server/forgetpassword/";
+	public static updatePassEmailId="http://10.151.61.152:8765/api/uaa-server/forgetpassword/";
 
 }
