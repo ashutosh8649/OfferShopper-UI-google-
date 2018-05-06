0cf55ac18bb23369136623a418a4aae9880673f1
ashutosh8649 <mishaashutosh108@gmail.com>
2018-05-06 21:12:45 +0530

    added test case for offer service

--- src/app/services/offers.service.spec.ts
+++ src/app/services/offers.service.spec.ts
@@ -1,15 +1,46 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async, getTestBed, ComponentFixture } from '@angular/core/testing';
+import {MockBackend, MockConnection} from '@angular/http/testing';
+import {HttpModule, ResponseOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import {Headers, BaseRequestOptions,Response,Http, XHRBackend, RequestMethod} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { OffersService } from './offers.service';
 
 describe('OffersService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [OffersService]
-    });
-  });
+  let mockBackend: MockBackend;
 
+  beforeEach(async() => {
+    TestBed.configureTestingModule({
+      providers: [OffersService,
+      MockBackend,
+      BaseRequestOptions,
+      {
+        provide: Http,
+        deps: [MockBackend, BaseRequestOptions],
+        useFactory:
+        (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+          return new Http(backend, defaultOptions);
+        }
+      }],
+      imports : [HttpClientModule,HttpModule],
+    });
+    mockBackend = getTestBed().get(MockBackend);
+
+  });
   it('should be created', inject([OffersService], (service: OffersService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should have getOffers function', inject([OffersService], (service: OffersService) => {
+    expect(service.getOffers).toBeTruthy();
+  }));
+
+  it('should have getAddress function', inject([OffersService], (service: OffersService) => {
+    expect(service.getAddress).toBeTruthy();
+  }));
+
+  it('should have addToCarrybag function', inject([OffersService], (service: OffersService) => {
+    expect(service.addToCarrybag).toBeTruthy();
   }));
 });
